72a7f612a5a01232683790ddc31e2e56c5bc726b
Fred Hunter <fhunter@Freds-Mac-mini.local>
2015-02-10 12:40:07 -0800

    added button

--- test1/ViewController.swift
+++ test1/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func myButton(sender: AnyObject) {
+        myLabel.text = "this is a test"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
